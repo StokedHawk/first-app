3c77426f87876784a96efc4cd739ce74056bee6b
Milla Kwan <millakwan12@gmail.com>
2020-07-16 10:51:11 -0700

    changed the text of our app

---
+++ test Gitub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test Gitub
+//
+//  Created by Milla Kwan on 7/16/20.
+//  Copyright © 2020 Milla Kwan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
